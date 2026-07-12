8c60ff2fccedc4d398474a7d1fec265633f95ffe
Pavan kumar <etcherlapavan2007@gmail.com>
2026-07-12 19:49:37 +0530

    Update maxascendingsunarray.py

--- maxascendingsunarray.py
+++ maxascendingsunarray.py
@@ -1,1 +1,12 @@
-
+n=int(input())
+arr1=list(map(int,input().split()))
+maxi=total=arr1[0]
+if n==1:
+    print(arr1[0])
+for i in range(n-1):
+    if arr1[i]<arr1[i+1]:
+        total+=arr1[i+1]
+    else:
+        total=arr1[i+1]
+    maxi=max(maxi,total)
+print(maxi)
