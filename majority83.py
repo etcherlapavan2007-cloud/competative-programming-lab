bc69500eb75db31b5069b2b6fab4d47859219ef4
Pavan kumar <etcherlapavan2007@gmail.com>
2026-07-12 19:01:14 +0530

    Update majority83.py

--- majority83.py
+++ majority83.py
@@ -1,1 +1,12 @@
-
+n=int(input())
+arr1=list(map(int,input().split()))
+maxi=n//2
+if n==0:
+    print(-1)
+for i in arr1:
+    if arr1.count(i)>maxi:
+        print(i)
+        break
+else:
+    print(-1)
+        
