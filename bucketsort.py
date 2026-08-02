817c78c3ff9660dc7ad4144ced43e898a091e045
Pavan kumar <etcherlapavan2007@gmail.com>
2026-08-02 19:46:45 +0530

    Update bucketsort.py

--- bucketsort.py
+++ bucketsort.py
@@ -1,13 +1,20 @@
 n = int(input())
 arr = list(map(float, input().split()))
-buckets = [[] for i in range(n)]
 if n == 0:
-        print([])
+    print([])
+    exit()
+buckets = [[] for _ in range(n)]
 for i in arr:
     index = int(i * n)
+    if index >= n:
+        index = n - 1
     buckets[index].append(i)
 res = []
-for j in buckets:
-    j.sort()
-    res.extend(j)
-print(*res)
+for bucket in buckets:
+    bucket.sort()
+    res.extend(bucket)
+for x in res:
+    if x.is_integer():
+        print(int(x), end=" ")
+    else:
+        print(f"{x:.2f}", end=" ")
