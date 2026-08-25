28be07c1ba28c5634dfd08ddd6831c48178606e1
Pavan kumar <etcherlapavan2007@gmail.com>
2026-08-25 10:04:22 +0530

    Update borderofstring.py

--- borderofstring.py
+++ borderofstring.py
@@ -1,16 +1,6 @@
 n=input()
-pf=[]
-sf=[]
-for i in range(len(n)-1):
-    pf.append(n[:i+1])
-for j in range(len(n)-1,0,-1):
-    sf.append(n[j:])
-maxi=0
-i,j=0,0
-while i<len(pf) and j<len(sf):
-    if pf[i]==sf[i]:
-        temp=pf[i]
-        maxi=max(maxi,len(temp))
-    i+=1
-    j+=1
+temp=""
+for i in range(1,len(n)):
+    if n[:i]==n[-i:]:
+        temp=n[:i]
 print(temp)
